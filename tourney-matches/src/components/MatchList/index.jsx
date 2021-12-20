7de373a87d24c93acf4cfba8453e0c60e66e26ab
Lisa Faggiolo <lisa.faggiolo@live.ca>
2021-12-20 14:48:59 -0700

    Debut small react project for tournement-matches

---
+++ tourney-matches/src/components/MatchList/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Match from '../Match';
+
+const MatchList = (props) => {
+
+  return(
+    <section className="PlayerList MatchList">
+   
+      <h1>Match list</h1>
+      
+      <Match/>
+
+    </section>
+  );
+}
+
+export default MatchList;
---
+++ tourney-matches/src/components/PlayerList/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Player from '../Player';
+
+const PlayerList = (props) => {
+
+  return(
+    <section className="PlayerList">
+      
+      <h1>Current participating players</h1>
+      
+      <Player/>
+
+    </section>
+  );
+}
+
+export default PlayerList;
