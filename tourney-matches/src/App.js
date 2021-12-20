6a1d9bf4a227124b21d6db2f1d8974a264b3d85f
Lisa Faggiolo <lisa.faggiolo@live.ca>
2021-12-20 14:54:26 -0700

    PlayerList && MatchList added to App.js

--- tourney-matches/src/App.js
+++ tourney-matches/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
 
 function App() {
   return (
     <div className="App">
-      
+      <hi>
+        Tourney Matches <span>Where Coding ans Tournaments found their Match!</span>
+      </hi>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
--- tourney-matches/src/components/Match/index.jsx
+++ tourney-matches/src/components/Match/index.jsx
@@ -5,9 +5,13 @@ const Match = (props) => {
 
   return(
     <article className="Match">
-      <h1>Joe <span>vs</span> Bob</h1>
-      {/* To be shown when there is a winner */}<h2>Joe is the winner by 1!</h2>
-      {/* To be shown when there is no winner */}<h2>No winners yet!</h2>
+      <h1>
+        Joe <span>vs</span> Bob
+      </h1>
+      {/* To be shown when there is a winner */}
+      <h2>Joe is the winner by 1!</h2>
+      {/* To be shown when there is no winner */}
+      <h2>No winners yet!</h2>
    </article>
   )
 };
